1cf1080e63ec5c37f46cb3f4a60728eda3a75df0
Charvi-code <83527936+Charvi-code@users.noreply.github.com>
2021-09-17 12:11:15 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,24 +1,33 @@
+difference= 0;
+rightWristX= 0;
+leftWristX= 0;
 function setup()
 {
     video= createCapture(VIDEO);
     video.size(550,500);
     canvas= createCanvas(550,550);
     canvas.position(560,150);
-    poseNet=ml5.poseNet(video,modelLoaded);
-    poseNet.on('pose',gotPoses);
-}
+    poseNet=ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose',gotPoses);    
+}   
 function modelLoaded()
 {
     console.log("PoseNet is loaded!");
 }
 function gotPoses(results)
 {
-    if (results.lenght>0)
+    if (results.length > 0)
     {
         console.log(results);
+        leftWristX= results[0].pose.leftWrist.x;
+        rightWristX= results[0].pose.rightWrist.x;
+        difference= floor(leftWristX - rightWristX);
     }
 }
 function draw()
 {
     background('#87CEEB');
+    textSize(difference);
+    fill('#FFE787');
+    text('Charvi', rightWristX, leftWristX);
 }
